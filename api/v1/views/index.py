d52f8f53d0f32becef6d34452882b240048a8f40
luffy981 <jhonsmith981@hotmail.com>
2022-02-07 19:43:33 -0500

    index fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """object app_views that returns a JSON: status: OK"""
 from flask import jsonify
-from ap1.v1.views import app_views
+from flask import Flask
+from api.v1.views import app_views
+
+
+app_views.url_map.strict_slashes = False
 
 
 @app_views.route('/status')
